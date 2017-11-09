514acfe8ad20f0024727f37ca26466ac38df9456
Eduardo Aguilar Trejo <eduardoag@oc0534216437.ibm.com>
2017-11-09 10:28:36 -0500

    fix2

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,16 @@
+def call() {
+    timestamps {
+        test()
+    } // End Timestamps
+} // End Call
+
+def call(agentNodeName) {
+    timestamps {
+        node(agentNodeName) {
+            test()
+        }
+    }
+}
 def test(){
   println "THIS IS A TEST"
 }
