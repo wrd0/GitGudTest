79dd8310bb37c4bde705a61535a3b49d4f4bf50b
Eduardo Aguilar Trejo <eduardoag@oc0534216437.ibm.com>
2017-11-16 12:11:14 -0500

    safe

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -5,9 +5,16 @@ def call(method) {
             case "send": 
                 sh "echo SLACK SEND"
             break;
+            case "safeSend":
+                safeSend methdo
+            break;
             default:
-                error
+                echo "no such method"
             break;
         }
     } // End Timestamps
 } // End Call
+
+def safeSend(String send){
+    echo "sending message: ${send}"
+}
